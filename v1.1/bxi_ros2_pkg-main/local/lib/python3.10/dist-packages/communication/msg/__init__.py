e212761293f4123fa3bc17c7049ec2c8aeeb1e42
Luckyt1 <1670827120@qq.com>
2025-10-08 13:07:43 +0800

    delete

--- v1.1/bxi_ros2_pkg-main/local/lib/python3.10/dist-packages/communication/msg/__init__.py
+++
@@ -1,4 +0,0 @@
-from communication.msg._actuator_cmds import ActuatorCmds  # noqa: F401
-from communication.msg._motion_commands import MotionCommands  # noqa: F401
-from communication.msg._state_estimator import StateEstimator  # noqa: F401
-from communication.msg._touch_sensor import TouchSensor  # noqa: F401
--- v1.1/bxi_ros2_pkg-main/local/lib/python3.10/dist-packages/communication/srv/__init__.py
+++
@@ -1,3 +0,0 @@
-from communication.srv._gait_switch import GaitSwitch  # noqa: F401
-from communication.srv._robot_reset import RobotReset  # noqa: F401
-from communication.srv._simulation_reset import SimulationReset  # noqa: F401
